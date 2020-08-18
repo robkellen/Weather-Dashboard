d93a4c359b3ce5de0f69722b5b3d9309ecf41075
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-17 23:35:58 -0700

    updated JS with AJAX call for current weather in city from input

--- script.js
+++ script.js
@@ -7,11 +7,37 @@ function getLocalStorage(key) {
 }
 
 //declaring HTML variables
-const geoLocation = {};
-const todaysDate = moment().format("MM-DD-YYYY");
+const geoLocation = document.getElementById("#todayForecast")
+const todaysDate = moment().format("M-DD-YYYY");
+
 
 //execute the following function after page is fully loaded
 $(document).ready(function () { 
   //adding current date to header of today's forecast
   $("#currentDate").text(todaysDate);
+
+  //event function to trigger the AJAX call for city that user searches
+  $("#findCity").on("click", function(event){
+    event.preventDefault();
+    //collecting the text from the input box
+    const city = $("#cityInput").val();
+
+    const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
+    //url for the AJAX call query including user input and API key
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",&appid=" + apiKey;
+    //running AJAX call to OpenWeatherMap API
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response){
+      //console logging queryURL
+      console.log(queryURL);
+      //console logging resulting object
+      console.log(response);
+
+    })
+  })
+
+
+  
 });

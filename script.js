4908a2660191a17789a3e642a613cb5376a86802
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-18 17:37:41 -0700

    scrapped.  Starting with fresh JS

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
-//access local storage
-function getLocalStorage(key) {
-  const value = localStorage.getItem(key);
-  if (value) {
-    $(`#text${key}`).text(value);
-  }
+//declaring HTML variables
+const geoLocation;
+const todaysDate = moment().format("M-DD-YYYY");
+const savedLocations = [];
+
+function setDefault (){
+  const 
+  if ()
 }
 
-//declaring HTML variables
-const geoLocation = document.getElementById("#todayForecast")
-const todaysDate = moment().format("M-DD-YYYY");
+  // //event function to trigger the AJAX call for city that user searches
+  // $("#findCity").on("click", function(event){
+  //   event.preventDefault();
+  //   //collecting the text from the input box
+  //   const city = $("#cityInput").val();
 
+  //   const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
+  //   //url for the AJAX call query including user input and API key
+  //   const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",&appid=" + apiKey;
+  //   //running AJAX call to OpenWeatherMap API
+  //   $.ajax({
+  //     url: queryURL,
+  //     method: "GET"
+  //   }).then(function(response){
+  //     //console logging queryURL
+  //     console.log(queryURL);
+  //     //console logging resulting object
+  //     console.log(response);
 
-//execute the following function after page is fully loaded
-$(document).ready(function () { 
-  //adding current date to header of today's forecast
-  $("#currentDate").text(todaysDate);
-
-  //event function to trigger the AJAX call for city that user searches
-  $("#findCity").on("click", function(event){
-    event.preventDefault();
-    //collecting the text from the input box
-    const city = $("#cityInput").val();
-
-    const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
-    //url for the AJAX call query including user input and API key
-    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",&appid=" + apiKey;
-    //running AJAX call to OpenWeatherMap API
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response){
-      //console logging queryURL
-      console.log(queryURL);
-      //console logging resulting object
-      console.log(response);
-
-    })
-  })
+  //   })
+  // })
 
 
   
-});

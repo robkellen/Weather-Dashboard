2cd50358c3b0c6c5dcdbf653d0a377ed461ddacd
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-18 23:06:50 -0700

    updating current progress...i'm stuck

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function geoLocation (){
 function startPoint (){
   //if user declines provide weather info for Cave Creek, AZ
   if (!navigator.geolocation) {
-    getWeather("Cave Creek");
+    getCurrent("Chicago");
   } else {
     navigator.geolocation.getCurrentPosition(allowed, denied);
   }
@@ -27,24 +27,26 @@ function allowed (position){
     url: queryURL,
     method: "GET"
   }).then(function(response){
-    //console logging queryURL
-    console.log(queryURL);
-    //console logging resulting object
-    console.log(response);
+    city = response.name;
     
 });
 }
 
 function denied (){
-  city = "Cave Creek"
-  getCurrent(city);
+    getCurrent("Chicago");
 }
 
 
-function getCurrent () {
-  const city = "chicago"
+function getCurrent(city) {
+  $("currentCity").empty();
   const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
   const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",&appid=" + apiKey;
+
+  // $("#findCity").on("click", function(){
+  //   event.preventDefault
+  //   const city = $("#cityInput").val().trim();
+
+  // })
 
   $.ajax({
     url: queryURL,
@@ -54,7 +56,12 @@ function getCurrent () {
     console.log(queryURL);
     //console logging resulting object
     console.log(response);
+    $("currentCity").append(response.name)
 
 });
 }
+
+
+// function saveCitySearch ()
+getCurrent();
 startPoint();

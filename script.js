4c4d858ab75b7cfea64212654708d8cee1b9132a
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-18 19:09:41 -0700

    function for inital weather to be displayed is set based on geolocation.  If user denies access it will default to Cave Creek

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-const savedLocations = [];
-const geoLocation = "";
+var savedLocations = [];
+var city;
 
+//ask user to allow access to location
+function geoLocation (){
+  navigator.geolocation();
+}
+
+function startPoint (){
+  //if user declines provide weather info for Cave Creek, AZ
+  if (!navigator.geolocation) {
+    getWeather("Cave Creek");
+  } else {
+    navigator.geolocation.getCurrentPosition(allowed, denied);
+  }
+}
+
+function allowed (position){
+  
+  //obtaining info for lat/long
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
+  const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
+  const queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + apiKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+    //console logging queryURL
+    console.log(queryURL);
+    //console logging resulting object
+    console.log(response);
+    
+});
+}
+
+function denied (){
+  city = "Cave Creek"
+  getCurrent(city);
+}
 
 
 function getCurrent () {
@@ -19,4 +57,4 @@ function getCurrent () {
 
 });
 }
-getCurrent();
+startPoint();

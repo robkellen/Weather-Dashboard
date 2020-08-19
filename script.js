0db79fc93849b83b1fd249f1a13b510121557de1
Rob Kellen <rob.h.kellen@gmail.com>
2020-08-18 18:35:07 -0700

    get current city weather ajax call successful

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-//declaring HTML variables
-const geoLocation;
-const todaysDate = moment().format("M-DD-YYYY");
 const savedLocations = [];
+const geoLocation = "";
 
-function setDefault (){
-  const 
-  if ()
+
+
+function getCurrent () {
+  const city = "chicago"
+  const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
+  const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",&appid=" + apiKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response){
+    //console logging queryURL
+    console.log(queryURL);
+    //console logging resulting object
+    console.log(response);
+
+});
 }
-
-  // //event function to trigger the AJAX call for city that user searches
-  // $("#findCity").on("click", function(event){
-  //   event.preventDefault();
-  //   //collecting the text from the input box
-  //   const city = $("#cityInput").val();
-
-  //   const apiKey = "858f8ba5cfd6fd435f1cd0d521850b4d"
-  //   //url for the AJAX call query including user input and API key
-  //   const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",&appid=" + apiKey;
-  //   //running AJAX call to OpenWeatherMap API
-  //   $.ajax({
-  //     url: queryURL,
-  //     method: "GET"
-  //   }).then(function(response){
-  //     //console logging queryURL
-  //     console.log(queryURL);
-  //     //console logging resulting object
-  //     console.log(response);
-
-  //   })
-  // })
-
-
-  
+getCurrent();
